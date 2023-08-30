d65a4df7aec72b73139476928467e08f38fb53ce
lexxai <lexxai@gmail.com>
2023-08-30 07:24:15 +0300

    added factorize_mul

--- factorize_sync.py
+++ factorize_sync.py
@@ -1,5 +1,23 @@
 from multiprocessing import cpu_count, process
 
+
+def factorize_one(n , max_number):
+    result_div = []
+    for i in range(1, max_number + 1):
+        if n % i == 0:
+            result_div.append(i)
+    return result_div
+
+
+def factorize_mul(*number):
+    max_number = max(number)
+    result: list[list[int]] = []
+    for n in number:
+        result_div = factorize_one(n , max_number)
+        result.append(result_div)
+    return tuple(result)
+
+
 def factorize(*number):
     max_number = max(number)
     result: list[list[int]] = []
@@ -13,7 +31,7 @@ def factorize(*number):
 
 
 def test_factorize():
-    a, b, c, d = factorize(128, 255, 99999, 10651060)
+    a, b, c, d = factorize_mul(128, 255, 99999, 10651060)
     assert a == [1, 2, 4, 8, 16, 32, 64, 128]
     assert b == [1, 3, 5, 15, 17, 51, 85, 255]
     assert c == [1, 3, 9, 41, 123, 271, 369, 813, 2439, 11111, 33333, 99999]
