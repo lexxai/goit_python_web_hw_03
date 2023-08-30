04ac83a31e288104a1ccf3a8bb0121ff13d17367
lexxai <lexxai@gmail.com>
2023-08-30 06:08:56 +0300

    added threads_max, verbose options

--- main.py
+++ main.py
@@ -11,12 +11,20 @@ def get_params():
     args_cli = argparse.ArgumentParser(description="File sorter")
     args_cli.add_argument("-s", "--source", required=True)
     args_cli.add_argument("-o", "--output", default="sort_result")
+    args_cli.add_argument(
+        "-t", "--threads", default=10, type=int, help="Max threads, default: 10"
+    )
+    args_cli.add_argument("-v", "--verbose", action="store_true")
+
     return vars(args_cli.parse_args())
 
 
-def sort_folder(folder : Path, output: Path, condition: Semaphore):
+def sort_folder(
+    folder: Path, output: Path, condition: Semaphore, verbose: bool = False
+) -> None:
     with condition:
-        logging.info(f"`Thread running for sort in {folder}")
+        if verbose:
+            logging.info(f"`Thread running for sort in {folder}")
         # pre sorting files to dict
         result = {}
         for el in folder.iterdir():
@@ -24,7 +32,7 @@ def sort_folder(folder : Path, output: Path, condition: Semaphore):
                 ext = el.suffix.lower()
                 if not ext:
                     continue
-                ext_items = result.get(ext,[])
+                ext_items = result.get(ext, [])
                 ext_items.append(el)
                 result[ext] = ext_items
         # physical copy founded files
@@ -34,9 +42,12 @@ def sort_folder(folder : Path, output: Path, condition: Semaphore):
             for el in ext_items:
                 destination_file = destination_path.joinpath(el.name)
                 if destination_file.exists():
-                    destination_file = destination_path.joinpath(f"{el.stem}_{uuid4()}{el.suffix}")
+                    destination_file = destination_path.joinpath(
+                        f"{el.stem}_{uuid4()}{el.suffix}"
+                    )
                 try:
-                    logging.info(f"`Thread copy {el} to {destination_file}")
+                    if verbose:
+                        logging.info(f"`Thread copy {el} to {destination_file}")
                     copyfile(el, destination_file)
                 except OSError as e:
                     logging.error(e)
@@ -53,26 +64,37 @@ def get_folders(source_path: Path) -> list[Path]:
 def main(args_cli: dict = None):
     source = args_cli.get("source")
     output = args_cli.get("output", "sort_result")
+    threads_max = args_cli.get("threads", 10)
+    verbose = args_cli.get("verbose", False)
+
     assert source is not None, "Source must be defined"
-    logging.info("Start search")
+    if verbose:
+        logging.info("Start search")
     folders = get_folders(Path(source))
     output_path = Path(output)
     threads = []
-    pool = Semaphore(1)
+    pool = Semaphore(threads_max)
     for num, folder in enumerate(folders):
-        th = Thread(name=f'Th-{num}',  target=sort_folder, args=(folder, output_path, pool))
+        th = Thread(
+            name=f"Th-{num}", target=sort_folder, args=(folder, output_path, pool, verbose)
+        )
         th.start()
         threads.append(th)
-    logging.info(f"Wait all {len(threads)} threads")
+    if verbose:
+        logging.info(f"Wait all {len(threads)} threads")
     [th.join() for th in threads]
-    logging.info("Finish")
+    if verbose:
+        logging.info("Finish")
     # print(folders)
 
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [ %(threadName)s ] %(message)s')
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.DEBUG, format="%(asctime)s [ %(threadName)s ] %(message)s"
+    )
     args = get_params()
+    threads_max = args.get("threads", 10)
     start_time = datetime.now()
     main(args_cli=args)
     duration = datetime.now() - start_time
-    logging.info(f"Duration : {duration}")
+    logging.info(f"Duration : {duration} with threads: {threads_max}")
